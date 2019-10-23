d1d44169918d01e57b7efea34359dd94e907fc5b
DinoPass <dpassanisi@foundationmedicine.com>
2019-10-23 14:58:27 -0700

    adding latest

--- server.js
+++ server.js
@@ -2,10 +2,35 @@ const express = require('express')
 const Kafka = require('node-rdkafka')
 const cors = require('cors')
 const app = express()
-
-const producerStream = new Kafka.createWriteStream({
+const client = Kafka.AdminClient.create({
+    'client.id': 'kafka-admin', 
     'metadata.broker.list': 'localhost:9092'
-}, {}, {topic: 'first_topic'})
+})
+const producer = new Kafka.HighLevelProducer({
+    'client.id': 'kafka-producer',
+    'metadata.broker.list': 'localhost:9092',
+    'dr_cb': true
+})
+let producerState = {ready: false, connected: true}
+// const producerStream = new Kafka.createWriteStream({
+//     'metadata.broker.list': 'localhost:9092'
+// }, {}, {topic: 'first_topic'})
+producer.connect()
+producer.setPollInterval(5000)
+// producer.flush(2000, (err, data) => {
+//     console.log(data)
+// })
+producer.on('delivery-report', (err, report) => {
+    console.log(report)
+})
+
+producer.on('ready', () => {
+  producerState.ready = true
+})
+
+producer.on('disconnected', () => {
+    producerState.connected = false
+})
 
 const consumerStream = new Kafka.createReadStream({
     'metadata.broker.list': 'localhost:9092', 'group.id': 'messages'
@@ -14,20 +39,49 @@ const consumerStream = new Kafka.createReadStream({
 app.use(cors())
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
+app.post('/createtopic', (req, res) => {
+    console.log(req.body)
+    client.createTopic({
+        topic: req.body.topicName,
+        num_partitions: req.body.partitions,
+        replication_factor: req.body.replicationFactor
+    }, err => {
+        if(err) {
+            console.log(err)
+        } else {
+            res.send('topic was created')
+        }
+    })
+})
 app.post('/producer', (req, res) => {
     console.log('producer is writing message')
     // Buffer - an array of binary data
-    const success = producerStream.write(Buffer.from(req.body.message))
-    if(success) {
-        console.log('message was sent')
+    console.log('producer endpoint is reached', producerState.ready)
+    if(producerState.ready) {
+
+    producer.produce('first_topic', null, Buffer.from(req.body.message), 'user1', Date.now(), 
+    (err, offset) => {
+        if(err) {
+            console.log('producer couldnt write to partition')
+            res.status(433).send('failed')
+        }
+        console.log('message was sent', offset)
         res.send('OK')
-    } else {
-        console.log('producer couldnt write to partition')
-        res.status(433).send('failed')
-    }
+    })
+} else {
+    res.status(422).send('producer not ready')
+}
+    // const success = producerStream.write(Buffer.from(req.body.message))
+    // if(success) {
+    //     console.log('message was sent')
+    //     res.send('OK')
+    // } else {
+    //     console.log('producer couldnt write to partition')
+    //     res.status(433).send('failed')
+    // }
 })
 
-const port = 3000
+const port = 5000
 const server = require('http').createServer(app)
 const io = require('socket.io')(server)
 
@@ -40,6 +94,6 @@ io.on('connection', (socket) => {
     console.log('user is connected')
 })
 server.listen(port, () => {
-    
+    console.log('server is running')
 })
 
--- src/App.js
+++ src/App.js
@@ -5,11 +5,16 @@ import logo from './logo.svg';
 import './App.css';
 // import Kafka from 'node-rdkafka'
 
-const socket = io.connect('http://localhost:3000')
+const socket = io.connect('http://localhost:5000')
 
 function App() {
   
-const [message, setMessage] = useState('') 
+const [message, setMessage] = useState('')
+const [topicState, setTopicState] = useState({
+  topicName: '',
+  replicationFactor: 1,
+  partitions: 1
+})
 
   useEffect(() => {
     socket.connect()
@@ -20,9 +25,23 @@ const [message, setMessage] = useState('')
        socket.disconnect()
      }
   }, [])
+
+  function sendTopic(e) {
+    e.preventDefault()
+    axios.post('http://localhost:5000/createtopic', topicState)
+      .then(res => {
+        if(res.status === 200) {
+          console.log(res.data)
+        }
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+
   function sendMessage(e) {
     e.preventDefault()
-    axios.post('http://localhost:3000/producer', {message: message})
+    axios.post('http://localhost:5000/producer', {message: message})
       .then((res) => {
         if(res.status === 200 ) {
           console.log(res.data)
@@ -35,6 +54,12 @@ const [message, setMessage] = useState('')
   return (
     <div className="App">
       <header className="App-header">
+        <form onSubmit={sendTopic} >
+          <input type="text" placeholder="topic name" onChange={(e) => {setTopicState({...topicState, topicName: e.target.value})}} />
+          <input type="number" placeholder="replication factor" defaultValue="1" onChange={(e) => {setTopicState({...topicState, replicationFactor: parseInt(e.target.value)})}} />
+          <input type="number" placeholder="partition" defaultValue="1" onChange={(e) => {setTopicState({...topicState, partitions: parseInt(e.target.value)})}}  />
+          <button type="submit">create topic</button>
+        </form>
         <img src={logo} className="App-logo" alt="logo" />
       <form onSubmit={sendMessage}>
        <input type='text' onChange={(e) => {
